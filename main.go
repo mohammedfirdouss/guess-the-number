1eb4e552008d1d749919959d56a24c4b39ed8474
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-02-04 13:53:02 +0000

    new game enroute

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the Number Guessing Game!")
+	fmt.Println("I'm thinking of a number between 1 and 100.")
+	fmt.Println("")
+}
